6b761769d321d1e934e5157b8c5b798198595217
SamCherry09 <samcherry2006@outlook.de>
2022-06-15 12:22:20 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,73 @@
+
+
+const express = require('express')
+const cors = require("cors")
+const querystring = require('querystring')
+const app = express()
+const port = 4000
+
+const USERS = [
+    {
+        "email": "ja@gmail.com" ,
+        "password": "123",
+        "name": "Sam",
+        "famName": "Cherry",
+        "id": "972365"
+    },
+    {
+        "email": "nein@gmail.com" ,
+        "password": "456",
+        "name": "Nic",
+        "famName": "Schaufler",
+        "id": "934875"
+    },
+    {
+        "email": "HAHAHA" ,
+        "password": "789",
+        "name": "Sandra",
+        "famName": "Heidelberg",
+        "id": "123456"
+    },
+    {
+        "email": "besterSchwertkämpfer@" ,
+        "password": "789",
+        "name": "Sandra",
+        "famName": "Heidelberg",
+        "id": "876542"
+    }
+]
+
+var bodyParser = require('body-parser')//add this
+
+app.use(bodyParser())//add this before any route or before using req.body
+
+// var corsOptions ={
+//     origin: "*"
+// }
+app.use(cors())
+app.post('/LogIn/', (req, res) => {
+    for (const user of USERS) {//For-of-loop over this.users
+         
+        if (req.body.email == user.email && req.body.password == user.password) {
+            res.send({"id":user.id});
+            return
+        }
+        
+    }
+    res.send({"id":""});
+})
+app.get('/user', (req, res) => {
+    for (const user of USERS) {//For-of-loop over this.users
+         
+        if (req.query.id == user.id) {
+            res.send({password: "*******",...user});
+            return
+        }
+        
+    }
+    res.send({"email": "Failed to load","password": "Failed to load","name":"Failed to load","famName":"Failed to load", "id":"Failed to load"});
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
